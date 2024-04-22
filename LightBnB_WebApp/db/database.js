64152e54a275b6270e6b33d9cdd37bcb28da4ef1
Lubi Islam <lubaba.islam91@gmail.com>
2024-04-21 22:09:36 -0400

    Refactor addProperty to insert new property details with SQL

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -173,11 +173,36 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+    .query(
+      `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`,
+      [
+        owner_id,
+        title,
+        description,
+        thumbnail_photo_url,
+        cover_photo_url,
+        cost_per_night,
+        street,
+        city,
+        province,
+        post_code,
+        country,
+        parking_spaces,
+        number_of_bathrooms,
+        number_of_bedrooms
+      ]
+    )
+    .then((result) => {
+      return result.rows[0]; // Return the saved property
+    })
+    .catch((err) => {
+      console.error("Error adding property:", err.message);
+      throw err; // Rethrow the error to handle it at the caller's level
+    });
 };
+
 
 module.exports = {
   getUserWithEmail,
